359a4db8c79a38f284e465957b7dfa34218e2a11
shterybai <andrewkennan2000@gmail.com>
2023-03-11 16:29:27 +0000

    Left-hand side navbar

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from flask import Flask, redirect, url_for, render_template
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route("/fyp")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/")
+def redirect():
+    return redirect(url_for("home"))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
