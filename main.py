113a0357ae314f2e063921bb283a0a620b00f04f
shterybai <andrewkennan2000@gmail.com>
2023-03-12 16:12:30 +0000

    Animations added

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def home():
 
         # Step 4: combine the values of the input fields into a string
         combined_words = f"{word1},{word2},{word3}"
-        # return combined_words
+        return combined_words
 
         # Step 5: make a POST request to the external API using the combined string
-        response = requests.post('https://external-api.com', data={'words': combined_words})
+        # response = requests.post('https://external-api.com', data={'words': combined_words})
 
         # Step 6: return a response to the user
         # return response.text
