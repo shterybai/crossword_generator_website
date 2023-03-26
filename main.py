2356df3c2c7eae619fafd2d36a4fcf93491775a4
shterybai <andrewkennan2000@gmail.com>
2023-03-26 20:31:44 +0100

    Tried many fixes; unsuccessful

--- main.py
+++ main.py
@@ -9,26 +9,25 @@ import requests
 
 app = Flask(__name__)
 app.secret_key = "g7r76VFiUyigIy*^RgiOIipghvi7hvuYgU65VJGI&879&3£%AsDIjklb"
+css_file = [r"C:\Users\andre\Documents\GitHub\crossword_generator_website\static\css\bootstrap.css"]
+js_file = r"C:\Users\andre\Documents\GitHub\crossword_generator_website\static\js\bootstrap.js"
+
 
 @app.route("/fyp", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
-        # Step 3: extract the values of the three input fields
+
         word1 = request.form['word1']
         word2 = request.form['word2']
         word3 = request.form['word3']
 
-        # Step 4: combine the values of the input fields into a string
         combined_words = f"{word1},{word2},{word3}"
-        # return combined_words
 
-        # Step 5: make a GET request to the external API using the combined string
-        response = requests.get(f'https://c2db-109-255-231-194.eu.ngrok.io/request?user_words={combined_words}')
+        response = requests.get(f'https://d1cd-109-255-231-194.eu.ngrok.io/request?user_words={combined_words}')
         session["api_data"] = response.json()
 
         print(response.json())
 
-        # Step 6: return a response to the user
         return render_template("display_crossword.html", crossword=response.json())
     else:
         return render_template("index.html")
@@ -69,7 +68,7 @@ def create_empty_pdf():
     # Convert the rendered HTML to a PDF
     path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
     config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-    pdf = pdfkit.from_string(rendered, False, configuration=config, options={"enable-local-file-access": ""})
+    pdf = pdfkit.from_string(rendered, False, configuration=config, options={"enable-local-file-access": ""}, css=css_file)
 
     # Create a response object with the PDF as the content
     pdf_response = make_response(pdf)
