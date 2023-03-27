229dcb582d79696552119a8d01ad6cbf4488a54f
shterybai <andrewkennan2000@gmail.com>
2023-03-27 17:10:06 +0100

    Error page working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 import datetime
 from datetime import timedelta
+import json
 import requests
 
 app = Flask(__name__)
@@ -11,23 +12,22 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
 @app.route("/fyp", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
-        # Step 3: extract the values of the three input fields
-        word1 = request.form['word1']
-        word2 = request.form['word2']
-        word3 = request.form['word3']
+        try:
+            word1 = request.form['word1']
+            word2 = request.form['word2']
+            word3 = request.form['word3']
 
-        # Step 4: combine the values of the input fields into a string
-        combined_words = f"{word1},{word2},{word3}"
-        # return combined_words
+            combined_words = f"{word1},{word2},{word3}"
 
-        # Step 5: make a GET request to the external API using the combined string
-        response = requests.get(f'https://d1cd-109-255-231-194.eu.ngrok.io/request?user_words={combined_words}')
-        session["api_data"] = response.json()
+            response = requests.get(f'https://667d-109-255-231-194.eu.ngrok.io/request?user_words={combined_words}')
+            session["api_data"] = response.json()
 
-        print(response.json())
+            print(response.json())
 
-        # Step 6: return a response to the user
-        return render_template("crossword.html", crossword=response.json())
+            return render_template("crossword.html", crossword=response.json())
+        except json.JSONDecodeError:
+            # If a JSONDecodeError occurs, raise a custom exception
+            raise InvalidDataException('Invalid JSON data')
     elif "api_data" in session:
         response = session["api_data"]
         return render_template("crossword.html", crossword=response)
@@ -55,25 +55,20 @@ def empty_crossword():
     return render_template('empty_crossword.html', crossword=response)
 
 
-# @app.route("/crossword_request", methods=["POST", "GET"])
-# def crossword_request():
-#     word1 = request.form['word1']
-#     word2 = request.form['word2']
-#     word3 = request.form['word3']
-#
-#     # Step 4: combine the values of the input fields into a string
-#     combined_words = f"{word1},{word2},{word3}"
-#     # return combined_words
-#
-#     # Step 5: make a GET request to the external API using the combined string
-#     response = requests.post(f'https://3eef-109-255-231-194.eu.ngrok.io/request?user_words={combined_words}')
-#
-#     return response.text
+class InvalidDataException(Exception):
+    def __init__(self, message):
+        self.message = message
 
 
-@app.route("/")
+@app.errorhandler(InvalidDataException)
+def handle_invalid_data_error(error):
+    # Render the error template with the error message
+    return render_template('error.html', message=error.message), 400
+
+
+@app.route('/')
 def redirect():
-    return redirect(url_for("home"))
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
